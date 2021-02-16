b145800956a04c18117e99acfab435dcccefde3c
brayanleo1 <57221030+brayanleo1@users.noreply.github.com>
2021-02-16 17:42:36 -0300

    Update 06-02-2021.v
    Resto dos teoremas exceto o teorema 5_3.

--- 06-02-2021.v
+++ 06-02-2021.v
@@ -194,15 +194,214 @@ Proof.
   intro Q.
   unfold not.
   intro h.
+  right.
+  destruct (L Q) as [q1 | q2].
+  apply q1.
+  unfold not in q2.
+  destruct h.
+  split.
+  intro i.
+  destruct q2.
+  admit.
+  apply q2.
+Admitted.
+
+Theorem t5_3 : forall P Q : Prop, P /\ Q -> ~(~P\/~Q).
+Proof.
+  intro P.
+  intro Q.
+  intro h.
+  unfold not.
+  intro i.
+  destruct i as [i1 | i2].
+  apply i1.
+  apply h.
+  apply i2.
+  apply h.
+Qed.
+
+Theorem t5_4 : forall P Q : Prop,~(~P\/~Q) -> P /\ Q.
+Proof.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
+  split.
+Abort.
+
+Theorem t5_4 : lem -> forall P Q : Prop, ~(~P\/~Q) -> P /\ Q.
+Proof.
+  intro L.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
+  split.
   destruct (L P) as [p1 | p2].
-  left.
   apply p1.
   unfold not in p2.
   destruct h.
+  left.
+  apply p2.
+  destruct (L Q) as [q1 | q2].
+  apply q1.
+  unfold not in q2.
+  destruct h.
+  right.
+  apply q2.
+Qed.
+
+Theorem t6_1 : forall P Q : Prop, ~(P \/ Q) -> (~P /\ ~Q).
+Proof.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
   split.
-  apply p2.
   intro i.
-  apply p2.
-  contradiction.
+  apply h.
+  left.
+  apply i.
+  intro j.
+  apply h.
+  right.
+  apply j.
+Qed.
+
+Theorem t6_2 : forall P Q : Prop, (~P /\ ~Q) -> ~(P \/ Q).
+Proof.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
   intro i.
-  apply p2.
+  destruct h as [h1 h2].
+  destruct i as [i1 | i2].
+  apply h1.
+  apply i1.
+  apply h2.
+  apply i2.
+Qed.
+
+Theorem t6_3 : forall P Q : Prop, ~(P /\ Q) -> (~Q \/ ~P).
+Proof.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
+  right.
+  intro i.
+  apply h.
+  split.
+  apply i.
+Abort.
+
+Theorem t6_3_fei : lem -> forall P Q : Prop, ~(P /\ Q) -> (~Q \/ ~P).
+Proof.
+  intro L.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
+  destruct (L Q) as [q1 | q2].
+  right.
+  intro i.
+  apply h.
+  split.
+  apply i.
+  apply q1.
+  unfold not in q2.
+  left.
+  apply q2.
+Qed.
+
+Theorem t6_4 : forall P Q : Prop, (~Q \/ ~P) -> ~(P /\ Q).
+Proof.
+  intro P.
+  intro Q.
+  unfold not.
+  intro h.
+  intro i.
+  destruct i as [i1 i2].
+  destruct h as [h1 | h2].
+  apply h1.
+  apply i2.
+  apply h2.
+  apply i1.
+Qed.
+
+Theorem t7_1 : forall P Q R : Prop, P /\ (Q \/ R) -> (P /\ Q) \/ (P /\ R).
+Proof.
+  intro P.
+  intro Q.
+  intro R.
+  intro h.
+  destruct h as [h1 h2].
+  destruct h2 as [h3 | h4].
+  left.
+  split.
+  apply h1.
+  apply h3.
+  right.
+  split.
+  apply h1.
+  apply h4.
+Qed.
+
+Theorem t7_2 : forall P Q R : Prop, (P /\ Q) \/ (P /\ R) -> P /\ (Q \/ R).
+Proof.
+  intro P.
+  intro Q.
+  intro R.
+  intro h.
+  destruct h as [h1 | h2].
+  destruct h1 as [p q].
+  split.
+  apply p.
+  left.
+  apply q.
+  destruct h2 as [p r].
+  split.
+  apply p.
+  right.
+  apply r.
+Qed.
+
+Theorem t7_3 : forall P Q R : Prop, P \/ (Q /\ R) -> (P \/ Q) /\ (P \/ R).
+Proof.
+  intro P.
+  intro Q.
+  intro R.
+  intro h.
+  destruct h as [p | qr].
+  split.
+  left.
+  apply p.
+  left. 
+  apply p.
+  destruct qr as [q r].
+  split.
+  right.
+  apply q.
+  right.
+  apply r.
+Qed.
+
+Theorem t7_4 : forall P Q R : Prop, (P \/ Q) /\ (P \/ R) -> P \/ (Q /\ R).
+Proof.
+  intro P.
+  intro Q.
+  intro R.
+  intro h.
+  destruct h as [pq pr].
+  destruct pq as [p | q].
+  left.
+  apply p.
+  destruct pr as [p | r].
+  left.
+  apply p.
+  right.
+  split.
+  apply q.
+  apply r.
+Qed.
